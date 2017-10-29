67748afb411b42ba7bc8c6936d0cefbc09ad38e5
Andrew Meakovski <meako689@gmail.com>
2017-10-29 14:59:25 +0200

    db:settings

--- newfire_tt/settings.py
+++ newfire_tt/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = 'newfire_tt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'users',
+        'USER': 'vagrant',
+        'PASSWORD': 'vagrant',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
